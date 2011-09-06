89b1f1bb8e8f857f5e4e16fa3df338608bd6732b
Yuto Hayamizu <y.hayamizu@gmail.com>
2011-09-07 03:34:17 +0900

    add Pages#home

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Volvox::Application.routes.draw do
 
   root :to => "pages#home"
 
+  match '/signup', :to => 'users#new'
+  match '/signin', :to => 'sessions#new'
+  match '/signout', :to => 'sessions#destroy'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,5 +1,14 @@
 require 'spec_helper'
 
 describe PagesController do
+  render_views
 
+  describe "home" do
+    it "should have link to sign up page" do
+      get :home
+      response.should have_selector("a",
+                                    :href => signup_path,
+                                    :content => "Sign up")
+    end
+  end
 end
