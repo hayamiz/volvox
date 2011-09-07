ce6b03cfd330e184de220a401edaba09babe6484
Yuto Hayamizu <y.hayamizu@gmail.com>
2011-09-08 05:49:28 +0900

    add attr_accessible notations for security

--- app/models/diary.rb
+++ app/models/diary.rb
@@ -1,5 +1,6 @@
 
 class Diary < ActiveRecord::Base
+  attr_accessible :title, :desc
   has_many :entries
 
   validates(:title,
--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,5 +1,6 @@
 
 class Entry < ActiveRecord::Base
+  attr_accessible :title, :content
   belongs_to :diary
   default_scope :order => 'entries.created_at DESC'
 
