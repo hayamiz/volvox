60bab263bd928c9955a8fd9a542d812997707fb8
Yuto Hayamizu <y.hayamizu@gmail.com>
2011-09-09 03:37:21 +0900

    check diary existence

--- app/models/authorship.rb
+++ app/models/authorship.rb
@@ -3,8 +3,16 @@ class Authorship < ActiveRecord::Base
   attr_accessible :diary_id
 
   belongs_to :author, :class_name => "User", :foreign_key => "user_id"
-  belongs_to :diary, :class_name => "Diary"
+  belongs_to :diary, :class_name => "Diary", :foreign_key => "diary_id"
 
   validates(:user_id, :presence => true)
   validates(:diary_id, :presence => true)
+  validate :diary_existence
+
+  private
+  def diary_existence
+    unless Diary.find_by_id(self.diary_id)
+      errors.add(:diary, "must be exist")
+    end
+  end
 end
--- spec/models/authorship_spec.rb
+++ spec/models/authorship_spec.rb
@@ -10,4 +10,23 @@ describe Authorship do
   it "should create an instance" do
     @authorship.save!
   end
+
+  describe "validation" do
+    it "should not create an instance without user_id" do
+      @authorship.user_id = nil
+      @authorship.should_not be_valid
+    end
+
+    it "should not create an instance without diary_id" do
+      @authorship.diary_id = nil
+      @authorship.should_not be_valid
+    end
+
+    it "should not create an instance with invalid diary_id" do
+      @authorship.diary_id = @diary.id + 1
+      lambda do
+        @authorship.save
+      end.should_not change(Authorship, :count)
+    end
+  end
 end
