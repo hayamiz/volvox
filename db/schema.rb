4dc975b8cbd7fcb038fe632209c97f72ff6d3069
Yuto Hayamizu <y.hayamizu@gmail.com>
2011-09-15 05:34:42 +0900

    updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110913234948) do
+ActiveRecord::Schema.define(:version => 20110914202648) do
 
   create_table "authorships", :force => true do |t|
     t.integer  "user_id"
@@ -62,6 +62,8 @@ ActiveRecord::Schema.define(:version => 20110913234948) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "opt_records", ["time"], :name => "opt_records_time_idx"
 
   create_table "users", :force => true do |t|
     t.string   "name"
