60377102c76c8f38dff1afee3f5c8f80f739151f
Yuto Hayamizu <y.hayamizu@gmail.com>
2011-09-20 16:46:26 +0900

    add test for opt_record#create

--- spec/controllers/opt_records_controller_spec.rb
+++ spec/controllers/opt_records_controller_spec.rb
@@ -4,6 +4,21 @@ describe OptRecordsController do
   render_views
 
   describe "create method" do
-    
+    before(:each) do
+      @user = Factory(:user)
+      @diary = Factory(:diary)
+      @column = Factory(:opt_column, :diary => @diary)
+      @attr = {
+        :time => Time.at(0),
+        :value => Marshal.dump("c#{@column.id}".to_sym => 10)
+      }
+    end
+
+    describe "for non-signed-in users" do
+      it "should redirect to sign in page" do
+        post :create, :diary_id => @diary, :opt_record => @attr
+        response.should redirect_to(signin_path)
+      end
+    end
   end
 end
