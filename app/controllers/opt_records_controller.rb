a0a8ae15a657143039f19301818e9570cf3e0813
Yuto Hayamizu <y.hayamizu@gmail.com>
2011-09-20 20:44:37 +0900

    added filter for opt_records#create

--- app/controllers/opt_records_controller.rb
+++ app/controllers/opt_records_controller.rb
@@ -1,2 +1,10 @@
 class OptRecordsController < ApplicationController
+  include DiariesChildrenHelper
+
+  before_filter(:authenticate, :only => [:create])
+  before_filter(:diary_exist)
+  before_filter(:author_of_diary, :only => [:create])
+
+  def create
+  end
 end
