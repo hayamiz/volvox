5112c4080e0d3bdea9bfc276bbc611488965fa07
Yuto Hayamizu <y.hayamizu@gmail.com>
2011-09-22 04:40:37 +0900

    done with opt_records#index, create

--- app/controllers/opt_records_controller.rb
+++ app/controllers/opt_records_controller.rb
@@ -5,6 +5,33 @@ class OptRecordsController < ApplicationController
   before_filter(:diary_exist)
   before_filter(:author_of_diary, :only => [:create])
 
+  def index
+    @opt_columns = @diary.opt_columns.all
+    @opt_records = @diary.opt_records.all
+  end
+
   def create
+    @opt_record = @diary.opt_records.build(:time => params[:opt_record][:time])
+    value = {}
+    @diary.opt_columns.all.each do |col|
+      if params[:opt_record][col.ckey]
+        case col.col_type
+        when OptColumn::COL_INTEGER
+          value[col.ckey] = params[:opt_record][col.ckey].to_i
+        when OptColumn::COL_FLOAT
+          value[col.ckey] = params[:opt_record][col.ckey].to_f
+        when OptColumn::COL_STRING
+          value[col.ckey] = params[:opt_record][col.ckey].to_s
+        end
+      end
+    end
+    @opt_record[:value] = value
+
+    if @opt_record.save
+      flash[:success] = "Added a record successfully"
+    else
+      flash[:error] = "Failed to create a record"
+    end
+    redirect_to diary_path(@diary)
   end
 end
--- spec/controllers/opt_records_controller_spec.rb
+++ spec/controllers/opt_records_controller_spec.rb
@@ -3,6 +3,35 @@ require 'spec_helper'
 describe OptRecordsController do
   render_views
 
+  describe "index method" do
+    before(:each) do
+      @user = Factory(:user)
+      @diary = Factory(:diary)
+      @cols = []
+      3.times do
+        @cols << Factory(:opt_column,
+                         :diary => @diary,
+                         :name => Factory.next(:col_name))
+      end
+      @records = []
+      10.times do
+        value = {}
+        @cols.each do |col|
+          value[col.ckey] = rand
+        end
+        @records << Factory(:opt_record,
+                            :diary => @diary,
+                            :value => value)
+      end
+    end
+
+    it "should show table" do
+      get :index, :diary_id => @diary
+      response_xpath("//table/tr/th").size.should == @cols.size + 1 # time + col names
+      response_xpath("//table/tr/td").size.should == (@cols.size + 1) * @records.size
+    end
+  end
+
   describe "create method" do
     before(:each) do
       @user = Factory(:user)
@@ -10,7 +39,7 @@ describe OptRecordsController do
       @column = Factory(:opt_column, :diary => @diary)
       @attr = {
         :time => Time.at(0),
-        :value => Marshal.dump("c#{@column.id}".to_sym => 10)
+        @column.ckey => 10
       }
     end
 
@@ -18,6 +47,46 @@ describe OptRecordsController do
       it "should redirect to sign in page" do
         post :create, :diary_id => @diary, :opt_record => @attr
         response.should redirect_to(signin_path)
+      end
+    end
+
+    describe "for signed-in users" do
+      before(:each) do
+        test_sign_in(@user)
+      end
+
+      it "should redirect to diary page" do
+        post :create, :diary_id => @diary, :opt_record => @attr
+        response.should redirect_to(diary_path(@diary))
+      end
+
+      it "should not create an instance" do
+        lambda do
+          post :create, :diary_id => @diary, :opt_record => @attr
+        end.should_not change(OptRecord, :count)
+      end
+
+      it "should show error flash message" do
+        post :create, :diary_id => @diary, :opt_record => @attr
+        flash[:error].should_not be_empty
+      end
+
+      describe "for authors" do
+        before(:each) do
+          @user.participate(@diary)
+        end
+
+        it "should create an instance" do
+          lambda do
+            post :create, :diary_id => @diary, :opt_record => @attr
+          end.should change(OptRecord, :count).by(1)
+        end
+
+        it "should show successful flash message" do
+          post :create, :diary_id => @diary, :opt_record => @attr
+          flash[:success].should_not be_nil
+          flash[:success].should_not be_empty
+        end
       end
     end
   end
